bf12fb4e3cb282f6268a31af230ceee24f9bc965
Rahul Prajapati <152829018+RahulPrajapati60@users.noreply.github.com>
2026-04-18 01:41:40 +0530

    Update Productpage.jsx

--- Frontend/src/pages/Productpage.jsx
+++ Frontend/src/pages/Productpage.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { productAPI } from "../src/api/Product.js";
+import { productAPI } from "../api/Product.js";
 import { useAuth } from "../context/AuthContext";
 
 const fmt = (n) => "₹" + Number(n).toLocaleString("en-IN");
